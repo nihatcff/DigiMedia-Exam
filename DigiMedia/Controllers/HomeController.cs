e60c09a6c5a20013493a8805758a8814af6136fe
Nihat Kerimi <nihatrkh-mpa101@code.edu.az>
2026-01-22 08:23:44 +0400

    Add project files.

---
+++ DigiMedia/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigiMedia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
